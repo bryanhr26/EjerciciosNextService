d0cba8ee3c4bbad112b57ff967fc0a101360b180
bryanhr26 <bryanhr26@gmail.com>
2025-02-27 13:46:59 +0100

    Implementado: - Agrupación de las funciones js. - Offcanvas funcional - Implementado abrir y cerrar el offcanvas.

---
+++ wwwroot/js/FuncionesJS.js
@@ -0,0 +1,77 @@
+﻿//MENSAJE VENTANA
+function mostrarAlerta(mensaje) {
+    console.log("Mensaje a mostrar en la alerta:", mensaje); // Verifica el mensaje que se pasa
+
+    const result = confirm(mensaje); // Ejecuta confirm()
+    console.log("Resultado de confirm():", result); // Muestra si es true o false
+
+    if (result) {
+        console.log("Alerta aceptada, invocando función Blazor...");
+        // Llamamos directamente al método estático de Blazor
+        DotNet.invokeMethodAsync('BlazorApp1', 'ActualizarMensajeOperacion')
+            .then(data => {
+                console.log("Función de Blazor ejecutada");
+            })
+            .catch(error => console.error("Error al invocar Blazor:", error));
+    } else {
+        console.log("El usuario canceló la alerta.");
+    }
+}
+
+//OFFCANVAS
+function CerrarOffcanvas(nombreOffCanvas) {
+
+    var miOffcanvas = document.getElementById(nombreOffCanvas);
+    var bsOffCanvas = new bootstrap.Offcanvas(miOffcanvas);
+    var miOffCanvasEsVisible = bsOffCanvas._element.classList.contains("show");
+
+    if (miOffCanvasEsVisible) {
+
+        const capasProtectoras = document.querySelectorAll('.offcanvas-backdrop.fade.show');
+
+        if (capasProtectoras != null) {
+
+            capasProtectoras.forEach(capa => {
+
+                if (capa.id) {
+                    capa.remove();
+                }
+            })
+        }
+
+        miOffcanvas?.classList.remove('show');
+        miOffcanvas?.classList.add('hide');
+
+    }
+
+}
+
+window.abrirOffCanvas = (nombreOffCanvas) => {
+
+    var miOffcanvas = document.getElementById(nombreOffCanvas);
+    var bsOffCanvas = new bootstrap.Offcanvas(miOffcanvas);
+    var miOffCanvasEsVisible = bsOffCanvas._element.classList.contains("show");
+
+    if (!miOffCanvasEsVisible) {
+        bsOffCanvas._backdrop._config.clickCallback = null;
+        bsOffCanvas.show();
+        const capasProtectoras = document.querySelectorAll('.offcanvas-backdrop.fade.show');
+
+        if (capasProtectoras != null) {
+
+            capasProtectoras.forEach(capa => {
+
+                if (!capa.id) {
+
+                    capa.id = `${nombreOffCanvas}-bs`;
+
+                    return;
+
+                }
+            });
+        }
+    }
+
+};
+
+//AÑADIR MÁS
--- wwwroot/js/offcanvas.js
+++ wwwroot/js/offcanvas.js
@@ -1,11 +1,18 @@
 ﻿function OpenOffcanvas(id) {
     var offcanvasElement = document.getElementById(id);
+    offcanvasElement?.classList.add('show');
+
+    // Asegurarte de que el Offcanvas se muestra correctamente usando Bootstrap JS
     var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
     offcanvas.show();
 }
 
 function CerrarOffcanvas(id) {
     var offcanvasElement = document.getElementById(id);
+    offcanvasElement?.classList.remove('show');
+    offcanvasElement?.classList.add('hide');
+
+    // Asegurarte de que el Offcanvas se muestra correctamente usando Bootstrap JS
     var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
     offcanvas.hide();
 }
